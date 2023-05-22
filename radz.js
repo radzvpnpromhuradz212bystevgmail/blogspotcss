6c4202965371e68a8105d0afddca70a327ccdc9a
radzvpnpromhuradz212bystevgmail <54478694+radzvpnpromhuradz212bystevgmail@users.noreply.github.com>
2023-05-22 12:07:30 +0800

    Create radz.js

---
+++ radz.js
@@ -0,0 +1,8 @@
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  // Apply syntax highlighting to all code blocks
+  document.querySelectorAll('pre code').forEach(function(block) {
+    hljs.highlightBlock(block);
+  });
+});
+</script>
